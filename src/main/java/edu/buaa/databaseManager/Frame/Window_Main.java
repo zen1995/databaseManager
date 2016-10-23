8dfe5b8ac140ec3292193aa1e49e1ec095304271
zen chen <chenzenian@live.com>
2016-10-23 22:38:24 +0800

    commit

--- src/main/java/edu/buaa/databaseManager/Frame/Window_Main.java
+++ src/main/java/edu/buaa/databaseManager/Frame/Window_Main.java
@@ -1,27 +1,33 @@
 package edu.buaa.databaseManager.Frame;
 
-	import java.awt.Toolkit;
 import java.sql.SQLException;
 
 import javax.swing.UIManager;
 
-
+import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
 
 import edu.buaa.databaseManager.database.DatabaseHelper;
 
-	public class Window_Main {
+public class Window_Main {
 
-		public static void main(String[] args) throws SQLException  {
-			// TODO Auto-generated method stub
-			
-		   
-			DatabaseHelper helper = new DatabaseHelper();
-			BaseWindow window = new BaseWindow(helper);
-			Thread thread = new Thread(window);  
-			thread.start();  
-			window.show();
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+
+		try {
+			org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+			BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.generalNoTranslucencyShadow;
+			org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+			UIManager.put("RootPane.setupButtonVisible", false);
+			BeautyEyeLNFHelper.translucencyAtFrameInactive = false;
+
+		} catch (Exception e) {
+			// TODO exception
 		}
-
+		DatabaseHelper helper = new DatabaseHelper();
+		BaseWindow window = new BaseWindow(helper);
+		Thread thread = new Thread(window);
+		thread.start();
+		window.show();
 	}
 
-
+}
